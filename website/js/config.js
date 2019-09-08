e3bee764e89a7bdfb1445d6848e8ef1f9c6217c4
HasanGilani <40214963+HasanGilani@users.noreply.github.com>
2019-09-08 19:42:14 +0500

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_9sk2JEOFq', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4h656k7j8ido5oet5924sbbia6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_gzlhzivQ2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '56puq8bomad5opb6oki68ljljf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://5pe4oi9dme.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://ltwszg1ore.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'rajesh.ramaswamy@netenrich.com'
